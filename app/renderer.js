50477324c5a2e0018fc998dd892d76a91f5150f4
Peter Wildberger <pete.wildberger@gmail.com>
2018-03-13 22:24:08 -0500

    list home directory

--- app/renderer.js
+++ app/renderer.js
@@ -3,3 +3,9 @@ const fs = require('fs');
 
 const files = fs.readdirSync(os.homedir());
 console.log(files);
+
+files.forEach(name => {
+  const file = document.createElement('li');
+  file.textContent = name;
+  document.body.appendChild(file);
+});
